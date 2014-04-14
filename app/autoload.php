bddb7a2f9abf677a49386c669512d94ce48618ed
romain59292 <romain59292>
2014-04-14 09:51:27 +0200

    db
    kk

--- app/autoload.php
+++ app/autoload.php
@@ -24,7 +24,8 @@ EOF;
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+    $loader
+        ->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
